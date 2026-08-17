544200ae2719757144f775c0db96edd942cb90a7
Leena Dany <67622202+Leena26@users.noreply.github.com>
2026-08-17 14:46:27 +0800

    Create database

---
+++ db.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.join(__dirname, 'data.sqlite');
+const db = new sqlite3.Database(dbPath);
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS expenses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT,
+    vendor TEXT,
+    amount REAL,
+    category TEXT,
+    raw_json TEXT
+  )`);
+});
+
+module.exports = db;
